ebc41b2af9a7551cd049bcaf5f1659d6d91e46f4
Matan Yemini <matan@engageli.com>
2022-06-03 16:20:29 +0300

    create hello world route for example

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const { helloWorldRouter } = require("./routes/hello-world.route");
+const { getAppPort } = require("./utils/config");
+
+const app = express();
+
+const appPort = getAppPort();
+
+// simple route for OpenTelemetry testing
+app.get("/", helloWorldRouter);
+
+app.listen(appPort, () => {
+    console.log("Listening for requests on port: ", appPort);
+});
